3d2822c4401df59a9b56603f15b2ec4487a82658
Yaroslav <yaroslavvasqa@gmail.com>
2023-07-09 17:50:02 +0300

    вdz33

---
+++ src/test/java/BookingDates.java
@@ -0,0 +1,13 @@
+import lombok.*;
+
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+
+public class BookingDates {
+    private String checkin;
+    private String checkout;
+}
